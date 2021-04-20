90a9e67f45024c857f3a30962ba2ca6ce0142f5b
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-21 01:21:44 +0200

    Added high level main components

---
+++ src/components/MapView/MapView.tsx
@@ -0,0 +1,11 @@
+import "./MapView.scss";
+
+import React from "react";
+
+interface OwnProps {}
+
+const MapView: React.FC<OwnProps> = () => {
+  return <div className='map-view-component'>Map View</div>;
+};
+
+export default MapView;
