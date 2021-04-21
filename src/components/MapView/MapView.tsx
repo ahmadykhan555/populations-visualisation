248ffce74013ae14a5d7715c7ff1eb88c8c89c3b
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-22 01:10:23 +0200

    Configure component to show filtered countries on map

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -1,18 +1,54 @@
 import "./MapView.scss";
 
-import React, { useEffect } from "react";
+import { ConnectedProps, connect } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { createMap, createMarker, createPopup } from "../../utility/map";
 
-import { createMap } from "../../utility/map";
+import { Country } from "../../models/country";
+import mapboxgl from "mapbox-gl";
 
-interface OwnProps {}
+interface OwnProps extends PropsFromRedux {}
 const CONTAINER_ID = "app-map-view-container";
 
-const MapView: React.FC<OwnProps> = () => {
+const MapView: React.FC<OwnProps> = ({ countriesData }) => {
+  const [map, setMap] = useState<mapboxgl.Map>();
+
   // onMount
   useEffect(() => {
-    createMap(CONTAINER_ID);
+    setMap(createMap(CONTAINER_ID));
   }, []);
+
+  useEffect(() => {
+    if (map && countriesData.length) {
+      const bounds = new mapboxgl.LngLatBounds();
+      countriesData.forEach((country: Country) => {
+        const [lat, lng] = country.latlng;
+        if (lat && lng) {
+          const marker = createMarker(
+            `marker-${country.numericCode}`,
+            new mapboxgl.LngLat(lng, lat)
+          );
+          marker
+            .setPopup(
+              createPopup(`<p>Hello from <strong>${country.name}</strong></p>`)
+            )
+            .addTo(map)
+            .togglePopup();
+          bounds.extend(marker.getLngLat());
+        }
+      });
+      map.fitBounds(bounds); // fit viewport to show all markers
+    }
+  }, [countriesData]);
+
   return <div id={CONTAINER_ID} className='map-view-component'></div>;
 };
 
-export default MapView;
+const mapStateToProps = (state: any) => ({
+  countriesData: state.allCountriesData.filteredForComparison,
+});
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connector(MapView);
