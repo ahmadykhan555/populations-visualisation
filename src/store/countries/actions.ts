22eabea3e25ea52431a8854e58162575150f0457
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-22 01:11:20 +0200

    Add corresponding filter data action

--- src/store/countries/actions.ts
+++ src/store/countries/actions.ts
@@ -4,7 +4,12 @@ export const setCountriesData = (data: Country[]) => ({
   type: CountryStateActions.SET_COUNTRIES_DATA,
   payload: [...data],
 });
+export const setCountriesForComparison = (data: Country[]) => ({
+  type: CountryStateActions.SET_FOR_COMPARISON,
+  payload: [...data],
+});
 
 export enum CountryStateActions {
   SET_COUNTRIES_DATA = "SET-COUNTRIES-DATA",
+  SET_FOR_COMPARISON = "SET-FOR-COMPARISON",
 }
