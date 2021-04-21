59da4e6672822973f12bd66f39bfcbb75ffe7afe
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-22 01:09:36 +0200

    By default select top 3 as filtered countries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 
+import {
+  setCountriesData,
+  setCountriesForComparison,
+} from "./store/countries/actions";
+
 import CountriesList from "./components/CountriesList/CountriesList";
 import { Country } from "./models/country";
 import GraphView from "./components/GraphView/GraphView";
 import MapView from "./components/MapView/MapView";
 import { connect } from "react-redux";
 import { getAllCountriesData } from "./API/countries";
-import { setCountriesData } from "./store/countries/actions";
 import { useEffect } from "react";
 
 const App: React.FC<any> = ({ dispatch }) => {
@@ -19,6 +23,7 @@ const App: React.FC<any> = ({ dispatch }) => {
     try {
       const allData: Country[] = await getAllCountriesData();
       dispatch(setCountriesData(allData));
+      dispatch(setCountriesForComparison(allData.slice(0, 3)));
     } catch (e) {
       // handle error
     }
