b7c7c94c6f6f50112eeef95b809fd1049f2ef499
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-21 02:43:54 +0200

    Connect countries list component to consume data from redux

--- src/components/CountriesList/CountriesList.tsx
+++ src/components/CountriesList/CountriesList.tsx
@@ -1,10 +1,26 @@
 import "./CountriesList.scss";
 
+import { ConnectedProps, connect } from "react-redux";
+
+import { Country } from "../../models/country";
 import React from "react";
 
-interface OwnProps {}
-const CountriesList: React.FC<OwnProps> = ({}) => {
-  return <div className='countries-list-component'>Countries List</div>;
+interface OwnProps extends PropsFromRedux {}
+const CountriesList: React.FC<OwnProps> = ({ countriesData }) => {
+  return (
+    <div className='countries-list-component'>
+      {countriesData.map((country: Country) => (
+        <p key={country.numericCode}>{country.name}</p>
+      ))}
+    </div>
+  );
 };
 
-export default CountriesList;
+const mapStateToProps = (state: any) => ({
+  countriesData: state.countriesData.data,
+});
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connector(CountriesList);
