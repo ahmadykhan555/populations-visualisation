6fbc366bc628b0fb3e7c3aece8ad09815255b0ef
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2021-04-23 04:43:18 +0200

    Remove unused files

--- src/components/CountriesView/components/FilteredCountries/FilteredCountries.tsx
+++ src/components/CountriesView/components/FilteredCountries/FilteredCountries.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { CountryListItem } from "..";
 import { FilteredCountriesProps } from "./types";
-import "./FilteredCountries.scss";
 
 const FilteredCountries: React.FC<FilteredCountriesProps> = ({ countries }) => {
   return (
